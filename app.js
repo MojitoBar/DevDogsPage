c47f95531f90aefbdb8ef1af326c553a0fe00b25
MojitoBar <wnehdtjr5@naver.com>
2018-11-10 16:49:51 +0900

    notice connected mysql table

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'devdogs'
+});
+
 app.set('view engine', 'jade');
 app.set('views', './views')
 // 정적인 파일을 불러오는 코드 (public 폴더를 기준으로 함)
@@ -10,6 +18,17 @@ app.get('/', function(req, res){
     res.render("index");
 })
 
+app.get('/Notice', function(req, res){
+    var sql = 'SELECT * FROM notice';
+    conn.query(sql, function(err, datas, fields){
+        if(err){
+            console.log(err);
+        }
+        console.log(datas);
+        res.render("notice", {datas:datas});
+    })
+})
+
 app.listen(3000, function(req, res){
-    console.log('Connected 3000 Port!')
+    console.log('Connected 3000 Port!');
 })
---
+++ notice_database.sql
@@ -0,0 +1,2 @@
+use devdogs;
+select * from notice;
