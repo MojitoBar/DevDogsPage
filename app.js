f6a7d19ece569ce01e9de93446e04eed0f0cf77a
MojitoBar <wnehdtjr5@naver.com>
2018-11-11 18:27:06 +0900

    Delete abstract

--- app.js
+++ app.js
@@ -18,51 +18,53 @@ app.set('views', './views')
 // 정적인 파일을 불러오는 코드 (public 폴더를 기준으로 함)
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render("index");
 })
 
-app.get('/Notice', function(req, res){
+app.get('/Notice', function (req, res) {
     var sql = 'SELECT * FROM notice';
-    conn.query(sql, function(err, datas, fields){
-        if(err){
+    conn.query(sql, function (err, datas, fields) {
+        if (err) {
             console.log(err);
         }
-        res.render("notice", {datas:datas});
+        res.render("notice", { datas: datas });
     })
 })
 
-app.get('/Notice/add', function(req, res){
+app.get('/Notice/add', function (req, res) {
     res.render("Notice_add")
 })
 
-app.post('/Notice/add', function(req, res){
+app.post('/Notice/add', function (req, res) {
     var title = req.body.title;
     var content = req.body.content;
     var write_time = moment().format("YY:MM:DD");
     var sql = 'INSERT INTO notice (title, content, write_time) VALUES(?, ?, ?)';
-    conn.query(sql, [title, content, write_time], function(err, rows){
-        if(err){
+    conn.query(sql, [title, content, write_time], function (err, rows) {
+        if (err) {
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        else{
+        else {
             res.redirect('/Notice/' + rows.insertId);
         }
     })
 })
 
-app.get('/Notice/:id', function(req, res){
+app.get('/Notice/:id', function (req, res) {
     var sql = 'SELECT * FROM notice where id=?';
     var id = req.params.id;
-    conn.query(sql, [id], function(err, data, fields){
-        if(err){
+    conn.query(sql, [id], function (err, data, fields) {
+        if (err) {
             console.log(err);
         }
-        res.render("notice_id", {data:data[0]});
+        res.render("notice_id", { data: data[0] });
     })
 })
 
-app.listen(3000, function(req, res){
+// app.post('')
+
+app.listen(3000, function (req, res) {
     console.log('Connected 3000 Port!');
 })
