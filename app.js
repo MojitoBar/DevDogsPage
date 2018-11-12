39d7335080b53b18d6ad6c55ff028de3985456f1
MojitoBar <wnehdtjr5@naver.com>
2018-11-12 17:17:00 +0900

    image & text file upload to mysql

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ var bodyParser = require('body-parser');
 var app = express();
 var moment = require('moment');
 
+// 파일 관련 모튤
+var multer = require('multer');
+
+// 파일 저장위치와 파일이름 설정
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        // 파일이 이미지 파일이면
+        if(file.mimetype == "image/jpeg" || file.mimetype == "image/jpg" || file.mimetype == "image/png"){
+            console.log('이미지 파일입니다.');
+            cb(null, 'uploads/images');
+        }
+        else if(file.mimetype == "application/pdf" || file.mimetype == "application/txt" || file.mimetype == "application/octet-stream"){
+            console.log('텍스트 파일입니다.');
+            cb(null, 'uploads/texts');
+        }
+    },
+    // 파일 이름 설정
+    filename: function(req, file, cb){
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+})
+
+var upload = multer({ storage: storage })
+
 var mysql = require('mysql');
 var conn = mysql.createConnection({
     host: 'localhost',
@@ -36,12 +60,13 @@ app.get('/Notice/add', function (req, res) {
     res.render("Notice_add")
 })
 
-app.post('/Notice/add', function (req, res) {
+app.post('/Notice/add', upload.single("fileupload"), function (req, res, next) {
     var title = req.body.title;
     var content = req.body.content;
     var write_time = moment().format("YY:MM:DD");
-    var sql = 'INSERT INTO notice (title, content, write_time) VALUES(?, ?, ?)';
-    conn.query(sql, [title, content, write_time], function (err, rows) {
+    var file_path = req.file.path;
+    var sql = 'INSERT INTO notice (title, content, write_time, user_file) VALUES(?, ?, ?, ?)';
+    conn.query(sql, [title, content, write_time, file_path], function (err, rows) {
         if (err) {
             console.log(err);
             res.status(500).send('Internal Server Error');
@@ -87,6 +112,23 @@ app.get('/Notice/edit/:id', function(req, res){
             res.render('notice_edit', { notice: notice[0] });
         }
     })
+})
+
+app.post('/Notice/edit/:id', function(req, res){
+    var id = req.params.id;
+    var title = req.body.title;
+    var content = req.body.content;
+    var write_time = moment().format("YY:MM:DD");
+    var sql = 'UPDATE notice SET title=?, content=?, write_time=? WHERE id=?'
+
+    conn.query(sql, [title, content, write_time, id], function(err, result){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/Notice/' + id);
+        }
+    });
 })
 
 app.listen(3000, function (req, res) {
